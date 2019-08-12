8fd34a3933679bb65ce7d1d8209cdcdd50ea23d9
Son Le <leson238@gmail.com>
2019-08-12 08:25:33 -0400

    build site ready

---
+++ precache-manifest.e9a3625af68941fdc7bb7bd03f282212.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658badf97a965bd5f10fdc8274dc9ba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75b3b99c12271c232021",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "75b3b99c12271c232021",
+    "url": "/static/js/main.38a8cf0c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
